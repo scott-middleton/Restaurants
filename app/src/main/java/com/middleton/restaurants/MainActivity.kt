e6beb53c8c851ad2c408cf1899feb8f6e817c031
Scott <mrscottmiddleton@gmail.com>
2023-02-03 15:38:28 +0000

    Initial commit

---
+++ app/src/main/java/com/middleton/restaurants/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.middleton.restaurants
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity: ComponentActivity() {
+}
